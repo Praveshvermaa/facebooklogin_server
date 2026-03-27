b3d897d6b24275e8b23db722104e4dd0965481fc
Pravesh Verma <praveshdhakad62@gmail.com>
2026-03-28 00:10:49 +0530

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const pageRoutes = require("./routes/pages");
 const app = express();
 
 // Middleware
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
